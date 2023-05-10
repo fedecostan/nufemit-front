e925b9c1e2627a95c13bfc4e95635e834f6c4bb3
Federico Costantino Cardu <costantino.fe@gmail.com>
2023-05-10 12:22:56 +0200

    Server configuration

--- js/requestHelper.js
+++ js/requestHelper.js
@@ -1,4 +1,4 @@
-const URL = 'http://localhost:8080/';
+const URL = 'https://app-nufemit-back.herokuapp.com/';
 
 function sendPostNoAuthorization(path, body, errorHandle, successHandle) {
     fetch(URL + path, {
